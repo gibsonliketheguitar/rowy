c7fea9ce5a752e0ea00615f2048cd97ef5048196
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-02-16 14:33:17 +0700

    add UserInvite components

--- src/pages/Settings/UserInvites.tsx
+++ src/pages/Settings/UserInvites.tsx
@@ -10,49 +10,18 @@ import {
   Collapse,
 } from "@mui/material";
 
-import FloatingSearch from "@src/components/FloatingSearch";
 import SlideTransition from "@src/components/Modal/SlideTransition";
 import UserItem from "@src/components/Settings/UserManagement/UserItem";
 import UserSkeleton from "@src/components/Settings/UserManagement/UserSkeleton";
-import InviteUser from "@src/components/Settings/UserManagement/InviteUser";
 
-import useCollection from "@src/hooks/useCollection";
-import useBasicSearch from "@src/hooks/useBasicSearch";
-import { USERS, USER_INVITES } from "@src/config/dbPaths";
-import UserInvites from "./UserInvites";
-
-const SEARCH_KEYS = ["id", "user.displayName", "user.email"];
-
-export interface User {
-  id: string;
-  user: {
-    displayName: string;
-    email: string;
-    photoURL: string;
-  };
-  roles?: string[];
-}
-
-export default function UserManagementPage() {
-  const [usersState] = useCollection({ path: USERS });
-  const [invitesState] = useCollection({ path: USER_INVITES });
-  const users: User[] = usersState.documents ?? [];
-  const loading = usersState.loading || !Array.isArray(usersState.documents);
-
-  const [results, query, handleQuery] = useBasicSearch(users, SEARCH_KEYS);
-
+export default function UserInvites({
+  loading,
+  query,
+  results,
+  usersState,
+}: any) {
   return (
-    <Container maxWidth="sm" sx={{ px: 1, pt: 1, pb: 7 + 3 + 3 }}>
-      <FloatingSearch
-        label="Search users"
-        onChange={(e) => handleQuery(e.target.value)}
-      />
-      <UserInvites
-        loading={loading}
-        query={query}
-        results={results}
-        usersState={invitesState}
-      />
+    <>
       <SlideTransition in timeout={100}>
         <Stack
           direction="row"
@@ -65,13 +34,10 @@ export default function UserManagementPage() {
             {!loading && query
               ? `${results.length} of ${usersState.documents.length}`
               : usersState.documents.length}{" "}
-            users
+            Pending User Invites
           </Typography>
-
-          <InviteUser />
         </Stack>
       </SlideTransition>
-
       {loading || (query === "" && results.length === 0) ? (
         <Fade in timeout={1000} style={{ transitionDelay: "1s" }} unmountOnExit>
           <Paper>
@@ -97,6 +63,6 @@ export default function UserManagementPage() {
           </Paper>
         </SlideTransition>
       )}
-    </Container>
+    </>
   );
 }
